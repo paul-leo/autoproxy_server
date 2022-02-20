1883f2a63e343be85d6a04c2233f51375045fb67
pengzai-dev <roc.liu@hotmail.com>
2022-02-20 22:13:38 +0800

    发布到阿里云

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,21 +1,11 @@
 module.exports = {
     name: 'AutoProxy',
-    script: 'npm install && npm run start',
+    script: 'npm run start',
     // 指定监听的文件夹
     // watch: ['server', 'client'],
     // 指定延迟时间
     watch_delay: 1000,
-    'pre-setup': 'pnpm install',
+    'pre-setup': 'npm install',
     // 指定要忽略的文件夹
     ignore_watch: ['node_modules'],
-    deploy: {
-        production: {
-            user: 'root',
-            host: ['121.43.183.161'],
-            ref: 'origin/master',
-            repo: 'https://github.com/IdeaNest-org/autoproxy_server.git',
-            path: '/var/www/autoproxy_server',
-            'post-deploy': 'npm install',
-        },
-    },
 };
