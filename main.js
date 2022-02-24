e3dd78108997e0a1b6b19d8b97f41ce50c5f34b4
pengzai-dev <roc.liu@hotmail.com>
2022-02-24 10:50:56 +0800

    更新默认节点

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ app.get('/getip', async (req, res) => {
 app.get('/proxy-config', (req, res) => {
     const result = {
         code: 0,
-        data: 'tls://sg-m.olg-food.icu:443,ws://@/fbi-open-the-door,vmess://2ab48700-5c25-384d-9680-bc389466f9dc@?alertID=2',
+        data: 'tls://hk-m9.olg-food.icu:443,ws://@/fbi-open-the-door,vmess://2ab48700-5c25-384d-9680-bc389466f9dc@?alertID=2',
     };
     res.send(JSON.stringify(result));
 });
