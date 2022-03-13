1be2f4d81ef0be2aa10e5476bdd28e5fcad69da8
pengzai-dev <roc.liu@hotmail.com>
2022-03-13 13:13:01 +0800

    设置日志最大条数

--- db/logs.js
+++ db/logs.js
@@ -3,6 +3,7 @@ import { Low, JSONFile } from 'lowdb';
 const file = join('./', '.db/log.json');
 const adapter = new JSONFile(file);
 const db = new Low(adapter);
+const maxLength = 100;
 const Logs = {
     async saveLog(appName = '', msg) {
         if (!appName) {
@@ -16,7 +17,11 @@ const Logs = {
             if (!db.data.logs[appName]) {
                 db.data.logs[appName] = [];
             }
-            db.data.logs[appName].push(msg);
+            const total = db?.data?.logs?.[appName].length;
+            if (total > maxLength) {
+                db?.data?.logs?.[appName].splice(maxLength, total - maxLength);
+            }
+            db.data.logs[appName].splice(0, 0, msg);
             await db.write();
             return true;
         } catch (error) {
@@ -32,6 +37,7 @@ const Logs = {
         if (!db.data) {
             db.data = { logs: {} };
         }
+
         return db?.data?.logs?.[appName] || [];
     },
 };
