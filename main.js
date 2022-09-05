278b56f18fa7681a282b1447a6c2cb01617d8daf
pengzai-dev <roc.liu@hotmail.com>
2022-09-05 08:21:48 +0700

    更新server

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ app.get('/getip', async (req, res) => {
 app.get('/proxy-config', (req, res) => {
     const result = {
         code: 0,
-        data: 'ss://aes-128-gcm:21dba017-30b2-4654-a2b3-9caf433d2b02@yum-install.centos.gatknqh.cn:11010',
+        data: 'ss://aes-128-gcm:21dba017-30b2-4654-a2b3-9caf433d2b02@yum-install.centos.gatknqh.cn:11011',
     };
     res.send(JSON.stringify(result));
 });
