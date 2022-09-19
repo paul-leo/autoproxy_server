f3d65bddc6a605ec812d40a45b8e3c3bcf32d59b
pengzai-dev <roc.liu@hotmail.com>
2022-09-19 22:12:18 +0800

    增加是否中文的判断

--- modules/airport/randomNode.js
+++ modules/airport/randomNode.js
@@ -1,9 +1,18 @@
 import parseAirport from './parseAirport.js';
 import AIRPORTS from './url.js';
 
+const cnKeyWords = ['hk', '香港', '新加坡', '台湾', '澳门'];
+function hasCnKeyword(label) {
+    return cnKeyWords.some((keyword) => {
+        return label.indexOf(keyword) > -1;
+    });
+}
 async function randomANode() {
     const randomIndex = Math.round(Math.random());
-    const nodeList = await parseAirport(AIRPORTS[randomIndex]);
+    let nodeList = await parseAirport(AIRPORTS[randomIndex]);
+    nodeList = nodeList.filter(({ label }) => {
+        return label && hasCnKeyword(label);
+    });
     const nodeIndex = parseInt(Math.random() * nodeList.length);
     return nodeList[nodeIndex];
 }
---
+++ modules/blackList/index.js
@@ -0,0 +1,2 @@
+//gwflist
+//https://raw.githubusercontent.com/Loyalsoldier/surge-rules/release/gfw.txt
