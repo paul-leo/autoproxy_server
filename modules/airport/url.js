31ad15bfb0c826801c47d86884b8fda1988ab31a
pengzai-dev <roc.liu@hotmail.com>
2023-02-20 22:26:12 +0800

    更换订阅地址

--- modules/airport/url.js
+++ modules/airport/url.js
@@ -1,5 +1,5 @@
 const AIRPORTS = [
     'https://fibsub-sys.cyou/link/Rmff2ZYNmpm7wBtw?list=shadowrocket',
-    'https://config.51catss.com/api/v1/client/subscribe?token=f6d5f2a2821c0c7095c8616b35a85030',
+    'https://centos.config-catnet.xyz/api/v1/client/subscribe?token=f6d5f2a2821c0c7095c8616b35a85030',
 ];
 export default AIRPORTS;
