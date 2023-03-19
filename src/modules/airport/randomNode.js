3d70ac98c7f4d9147ed32e37f1a346e1599ba300
pengzai-dev <roc.liu@hotmail.com>
2023-03-19 21:41:20 +0800

    优化获取节点

--- src/modules/airport/randomNode.js
+++ src/modules/airport/randomNode.js
@@ -1,21 +1,56 @@
-import parseAirport from './parseAirport.js';
-import AIRPORTS from './url.js';
-
-const cnKeyWords = ['hk', '香港', '新加坡', '台湾', '澳门', 'jp', 'sg'];
+import getAllNode from './getAllNode.js';
+import { tcpPingPort } from 'tcp-ping-port';
+let avaliUrl = [];
+const cnKeyWords = ['hk', '香港', '新加坡', '台湾', '澳门', 'jp', 'sg', 'tw'];
 function hasCnKeyword(label) {
     return cnKeyWords.some((keyword) => {
         return label.indexOf(keyword) > -1;
     });
 }
 
+async function testNode(node) {
+    const { host, port } = node;
+    const options = {
+        socketTimeout: 3000,
+        dnsTimeout: 3000,
+    };
+
+    const res = await tcpPingPort(host, Number(port), options);
+    if (res.online) {
+        return node;
+    } else {
+        throw new Error('timeout');
+    }
+}
+
 async function randomANode() {
-    const randomIndex = Math.round(Math.random());
-    let nodeList = await parseAirport(AIRPORTS[randomIndex]);
+    if (avaliUrl.length > 5) {
+        try {
+            let res = await raceANode(avaliUrl);
+            if (res) {
+                return res;
+            } else {
+                avaliUrl = [];
+            }
+        } catch (error) {
+            // console.log(error);
+        }
+    }
+    let nodeList = await getAllNode();
     nodeList = nodeList.filter(({ label }) => {
         return label && hasCnKeyword(label);
     });
-    const nodeIndex = parseInt(Math.random() * nodeList.length);
-    return nodeList[nodeIndex];
+    const res = raceANode(nodeList);
+    avaliUrl.push(res);
+    return res;
 }
 
+async function raceANode(nodeList) {
+    const promisePool = [];
+    for (var i in nodeList) {
+        promisePool.push(testNode(nodeList[i]));
+    }
+    const res = await Promise.race(promisePool);
+    return res;
+}
 export default randomANode;
