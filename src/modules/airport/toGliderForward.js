9ca1379990784ef4d91b40595eece081fbf7f2ce
pengzai-dev <roc.liu@hotmail.com>
2023-03-19 21:07:43 +0800

    修复trojan 错误

--- src/modules/airport/toGliderForward.js
+++ src/modules/airport/toGliderForward.js
@@ -1,9 +1,10 @@
 export default function toGliderForward(node) {
     const { protocol, password, encrypt, host, port, params } = node || {};
+    const { peer } = params || {};
     if (host) {
         switch (protocol) {
             case 'trojan':
-                return `trojan://${password}@${host}:${port}`;
+                return `trojan://${password}@${host}:${port}?serverName=${peer}`;
             case 'ss':
                 return `ss://${encrypt}:${password}@${host}:${port}`;
         }
