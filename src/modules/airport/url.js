325503a09af6617312207a4ac358c4ad307f377f
pengzai-dev <roc.liu@hotmail.com>
2023-03-28 11:18:17 +0800

    更新

--- src/modules/airport/url.js
+++ src/modules/airport/url.js
@@ -1,5 +1,5 @@
 const AIRPORTS = [
     'https://fibsub-sys.cyou/link/Rmff2ZYNmpm7wBtw?list=shadowrocket',
-    'https://update-debian.config-catnet.xyz/api/v1/client/subscribe?token=87624f280ce7889d3e057bd379d30c90',
+    'https://update-centos.config-catnet.xyz/api/v1/client/subscribe?token=87624f280ce7889d3e057bd379d30c90',
 ];
 export default AIRPORTS;
