60ba3a1290523f582982a0e1a7d016a570c0b968
pengzai-dev <roc.liu@hotmail.com>
2023-05-17 14:00:39 +0800

    删除多余的url

--- src/modules/airport/url.js
+++ src/modules/airport/url.js
@@ -1,6 +1,5 @@
 const AIRPORTS = [
-    'https://fibsub-sys.cyou/link/Rmff2ZYNmpm7wBtw?list=shadowrocket',
-    'https://update-centos.config-catnet.xyz/api/v1/client/subscribe?token=87624f280ce7889d3e057bd379d30c90',
+    'https://update-debian.config-catnet.xyz/api/v1/client/subscribe?token=893ab7239e0f808b660302bdcae09ee3',
 ];
 export default AIRPORTS;
 
