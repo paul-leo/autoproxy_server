3622d7fa4462260ef6f1722b3ab17ba751f4d3e4
pengzai-dev <roc.liu@hotmail.com>
2023-05-23 20:50:47 +0800

    更新fib

--- src/modules/airport/url.js
+++ src/modules/airport/url.js
@@ -1,6 +1,6 @@
 const AIRPORTS = [
-    'https://update-debian.config-catnet.xyz/api/v1/client/subscribe?token=893ab7239e0f808b660302bdcae09ee3'
-    // 'https://fibsub.cloud/link/Rmff2ZYNmpm7wBtw?list=shadowrocket'
+    'https://update-debian.config-catnet.xyz/api/v1/client/subscribe?token=893ab7239e0f808b660302bdcae09ee3',
+    'https://fibsub.cloud/link/Rmff2ZYNmpm7wBtw?list=shadowrocket'
 ];
 export default AIRPORTS;
 
