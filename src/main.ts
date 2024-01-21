babdfdf808fd90dafe8a5b498f69435b756e1d15
pengzai-dev <roc.liu@hotmail.com>
2024-01-21 14:42:22 +0800

    删除多余的服务

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,6 @@ import SMS from './controller/sms';
 import http from 'http';
 import cors from 'cors';
 import Paper from './helpers/db/paper.js';
-import autoInstall from './modules/autoinstall';
 process.env.TZ = 'Asia/Shanghai';
 const app = express();
 
@@ -38,34 +37,35 @@ app.use(
 );
 new Intranet(io);
 
-app.get('/saveip', async (req, res) => {
-    const { key, ip } = req.query;
-    const result = { code: 0, msg: '' };
-    if (!key) {
-        result.code = -1;
-        result.msg = 'key不能为空';
-    } else {
-        const saveRes = await DB.saveIP(key, ip);
-        if (!saveRes) {
-            result.code = -2;
-            result.msg = '保存失败';
-        }
-    }
-    await res.send(JSON.stringify(result));
-});
+// app.get('/saveip', async (req, res) => {
+//     const { key, ip } = req.query;
+//     const result = { code: 0, msg: '' };
+//     if (!key) {
+//         result.code = -1;
+//         result.msg = 'key不能为空';
+//     } else {
+//         const saveRes = await DB.saveIP(key, ip);
+//         if (!saveRes) {
+//             result.code = -2;
+//             result.msg = '保存失败';
+//         }
+//     }
+//     await res.send(JSON.stringify(result));
+// });
 
-app.get('/getip', async (req, res) => {
-    const { key } = req.query;
-    const result = { code: 0, msg: '', data: null };
-    const ip = await DB.getIP(key);
-    if (!ip) {
-        result.code = -1;
-        result.msg = '获取失败';
-    } else {
-        result.data = { ip };
-    }
-    await res.send(JSON.stringify(result));
-});
+// app.get('/getip', async (req, res) => {
+//     const { key } = req.query;
+//     const result = { code: 0, msg: '', data: null };
+//     const ip = await DB.getIP(key);
+//     if (!ip) {
+//         result.code = -1;
+//         result.msg = '获取失败';
+//     } else {
+//         result.data = { ip };
+//     }
+//     await res.send(JSON.stringify(result));
+// });
+
 app.get('/proxy-config', async (req, res) => {
     const forward = toGliderForward(await randomANode());
     const result = {
@@ -75,47 +75,47 @@ app.get('/proxy-config', async (req, res) => {
     res.send(JSON.stringify(result));
 });
 
-app.get('/all-proxy', async (req, res) => {
-    const list = await getAllNode();
-    const data = list
-        .map((item) => {
-            return toGliderForward(item);
-        })
-        .filter((item) => item);
-    const result = {
-        code: 0,
-        data,
-    };
-    res.send(JSON.stringify(result));
-});
+// app.get('/all-proxy', async (req, res) => {
+//     const list = await getAllNode();
+//     const data = list
+//         .map((item) => {
+//             return toGliderForward(item);
+//         })
+//         .filter((item) => item);
+//     const result = {
+//         code: 0,
+//         data,
+//     };
+//     res.send(JSON.stringify(result));
+// });
 
-app.get('/pac', async (req, res) => {
-    const { key } = req.query;
-    var pacContent = await fs.readFile('./pac.js', {
-        encoding: 'utf8',
-    });
-    const ip = await DB.getIP(key);
-    pacContent = pacContent.replace(
-        /__DEV_PROXY__/gi,
-        ip ? 'PROXY ' + ip : 'DIRECT'
-    );
-    res.send(pacContent);
-});
+// app.get('/pac', async (req, res) => {
+//     const { key } = req.query;
+//     var pacContent = await fs.readFile('./pac.js', {
+//         encoding: 'utf8',
+//     });
+//     const ip = await DB.getIP(key);
+//     pacContent = pacContent.replace(
+//         /__DEV_PROXY__/gi,
+//         ip ? 'PROXY ' + ip : 'DIRECT'
+//     );
+//     res.send(pacContent);
+// });
 
-app.post('/log', async (req, res) => {
-    const { msg, appname } = req.body || {};
-    try {
-        const logList = await Logs.saveLog(appname, {
-            msg,
-            ip: req.ip,
-            time: new Date().toLocaleString(),
-        });
-        res.sendStatus(logList ? 200 : 400);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
-});
+// app.post('/log', async (req, res) => {
+//     const { msg, appname } = req.body || {};
+//     try {
+//         const logList = await Logs.saveLog(appname, {
+//             msg,
+//             ip: req.ip,
+//             time: new Date().toLocaleString(),
+//         });
+//         res.sendStatus(logList ? 200 : 400);
+//     } catch (error) {
+//         console.log(error);
+//         res.sendStatus(500);
+//     }
+// });
 
 app.post('/saveOrUpdatePaperDetail', async (req, res) => {
     try {
@@ -131,32 +131,32 @@ app.post('/saveOrUpdatePaperDetail', async (req, res) => {
     }
 });
 
-app.get('/log', async (req, res) => {
-    const { appname } = req.query;
-    try {
-        const msg = await Logs.getLogs(appname);
-        res.send(JSON.stringify({ data: msg, code: 200 }));
-    } catch (error) {
-        console.log(error);
-        res.send(JSON.stringify({ msg: error, code: 400 }));
-    }
-});
+// app.get('/log', async (req, res) => {
+//     const { appname } = req.query;
+//     try {
+//         const msg = await Logs.getLogs(appname);
+//         res.send(JSON.stringify({ data: msg, code: 200 }));
+//     } catch (error) {
+//         console.log(error);
+//         res.send(JSON.stringify({ msg: error, code: 400 }));
+//     }
+// });
 
-app.get('/addBlackDomain', async (req, res) => {
-    const { domain = '', tag = 'domains' } = req.query;
-    const result = { code: 0, msg: '保存成功' };
-    if (!domain) {
-        result.code = -1;
-        result.msg = 'domain不能为空';
-    } else {
-        const saveRes = await addBlackDomain(domain, tag);
-        if (!saveRes) {
-            result.code = -2;
-            result.msg = '保存失败';
-        }
-    }
-    await res.send(JSON.stringify(result));
-});
+// app.get('/addBlackDomain', async (req, res) => {
+//     const { domain = '', tag = 'domains' } = req.query;
+//     const result = { code: 0, msg: '保存成功' };
+//     if (!domain) {
+//         result.code = -1;
+//         result.msg = 'domain不能为空';
+//     } else {
+//         const saveRes = await addBlackDomain(domain, tag);
+//         if (!saveRes) {
+//             result.code = -2;
+//             result.msg = '保存失败';
+//         }
+//     }
+//     await res.send(JSON.stringify(result));
+// });
 
 app.get('/removeBlackDomain', async (req, res) => {
     const { domain = '', tag = 'domains' } = req.query;
